c8a61c224c1bea84d865bf15ccc0eec444627394
smartdev-0205 <smartdevpro001@gmail.com>
2024-05-06 21:05:13 +0000

    first commit

---
+++ programs/coffee-bean/src/lib.rs
@@ -0,0 +1,32 @@
+use anchor_lang::prelude::*;
+
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod states;
+pub mod utils;
+
+use instructions::*;
+
+declare_id!("HQtuvPE1boXZvbg1uiiSjzm6HQNUbkqShD6bmKabpiVe");
+
+#[program]
+pub mod coffee_bean {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>, new_authority: Pubkey) -> Result<()> {
+        initialize::handle(ctx, new_authority)
+    }
+
+    pub fn buy_eggs(ctx: Context<BuyEggs>, amount: u64) -> Result<()> {
+        buy_eggs::handle(ctx, amount)
+    }
+
+    pub fn sell_eggs(ctx: Context<SellEggs>) -> Result<()> {
+        sell_eggs::handle(ctx)
+    }
+
+    pub fn hatch_eggs(ctx: Context<HatchEggs>) -> Result<()> {
+        hatch_eggs::handle(ctx)
+    }
+}
